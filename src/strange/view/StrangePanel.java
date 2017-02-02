0ccb715fb6199c9f6e00f71ac380987e882f4019
Asmi0816 <Kidwriter2020@gmail.com>
2017-02-01 17:35:13 -0700

    Added to the deign and functionality of the game I will be able to start on story soon.

--- src/strange/view/StrangePanel.java
+++ src/strange/view/StrangePanel.java
@@ -1,27 +1,45 @@
 package strange.view;
 import javax.swing.*;
 import strange.controller.StrangeController;
-import java.awt.Event.*;
+import java.awt.event.*;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+
 
 public class StrangePanel extends JPanel
 {
 	private StrangeController baseController;
+	private SpringLayout baseLayout;
 	private JButton rightButton;
 	private JButton leftButton;
-	private SpringLayout baseLayout;
-	private JTextField storyBox;
-	private ImageIcon backGround;
+	private JLabel strangeLabel;
+	private JLabel storyBox;
+	private JLabel foodLabel;
+	private JLabel healthLabel;
+	private JLabel waterLabel;
+	private ImageIcon strangeGround;
+	
 	
 	public StrangePanel(StrangeController baseController)
 	{
 		this.baseController = baseController;
-		this.rightButton = new JButton("This is a spot you can press to affect the game.");
-		this.leftButton = new JButton("This is a spot you can press to affect the game.");
 		this.baseLayout = new SpringLayout();
-		this.storyBox = new JTextField(5);
-		this.backGround = new ImageIcon();
+		this.rightButton = new JButton("This is a spot you can press to affect the game.");
+		
+		
+		this.leftButton = new JButton("This is a spot you can press to affect the game.");
+		
+		this.foodLabel = new JLabel("FOOD: ");
+		this.healthLabel = new JLabel("HEALTH: ");
+		this.waterLabel = new JLabel("WATER: ");
+		this.storyBox = new JLabel("Press either button to start the game.");
+		
+		
+		this.strangeLabel = new JLabel();
+		strangeLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		this.strangeGround = new ImageIcon(getClass().getResource("/strange/view/images/mountainBackdrop.jpg"));
+		
 		
 		setupPanel();
 		setupLayout();
@@ -33,22 +51,62 @@ public class StrangePanel extends JPanel
 	{
 		this.setLayout(baseLayout);
 		this.setPreferredSize(new Dimension(900, 600));
-		this.setBackground(Color.DARK_GRAY);
 		
-		
+
 		this.add(leftButton);
 		this.add(rightButton);
 		this.add(storyBox);
+		this.add(strangeLabel);
+		this.add(healthLabel);
+		this.add(foodLabel);
+		this.add(waterLabel);
+		strangeLabel.setIcon(strangeGround);
 		
 	}
 	
 	public void setupLayout()
 	{
-		
+		baseLayout.putConstraint(SpringLayout.NORTH, rightButton, 0, SpringLayout.NORTH, leftButton);
+		baseLayout.putConstraint(SpringLayout.SOUTH, rightButton, 0, SpringLayout.SOUTH, leftButton);
+		storyBox.setFont(new Font("AR DARLING", Font.PLAIN, 16));
+		baseLayout.putConstraint(SpringLayout.EAST, rightButton, -50, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, foodLabel, 125, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, leftButton, -707, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, leftButton, -354, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, storyBox, -485, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, leftButton, 77, SpringLayout.SOUTH, storyBox);
+		leftButton.setFont(new Font("AR CENA", Font.PLAIN, 17));
+		rightButton.setFont(new Font("AR CENA", Font.PLAIN, 17));
+		baseLayout.putConstraint(SpringLayout.NORTH, strangeLabel, 12, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, strangeLabel, 12, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, leftButton, 50, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, strangeLabel, -12, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, strangeLabel, -12, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, storyBox, 100, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, storyBox, 200, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, healthLabel, 45, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, storyBox, -200, SpringLayout.EAST, this);
+		storyBox.setHorizontalAlignment(SwingConstants.CENTER);
 	}
 	
 	public void setupListeners()
 	{
+		rightButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent selection)
+			{
+				int selected = 1;
+			}
+
+		});
 		
+		leftButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent selection)
+			{
+				int selected = 0;
+			}
+
+		});
 	}
 }
