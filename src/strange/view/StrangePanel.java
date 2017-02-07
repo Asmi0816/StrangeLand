775be543749fab9b0631bebf316ffcff9eb3c5ff
Asmi0816 <Kidwriter2020@gmail.com>
2017-02-07 15:04:24 -0700

    Figured out how to uses the model and realized that the game wasn't supposed to be part of model also I set up a new timer class and I'm goi

--- src/strange/view/StrangePanel.java
+++ src/strange/view/StrangePanel.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Image;
 
 
 public class StrangePanel extends JPanel
@@ -18,6 +19,7 @@ public class StrangePanel extends JPanel
 	private JButton rightButton;
 	private JButton leftButton;
 	private JLabel strangeLabel;
+	private JLabel mapLabel;
 	private JLabel storyBox;
 	private JLabel foodLabel;
 	private JLabel healthLabel;
@@ -26,6 +28,8 @@ public class StrangePanel extends JPanel
 	private JLabel healthStatus;
 	private JLabel waterStatus;
 	private ImageIcon strangeGround;
+	private ImageIcon userMap;
+	private ImageIcon newsPaper;
 	
 	
 	public StrangePanel(StrangeController baseController)
@@ -34,12 +38,15 @@ public class StrangePanel extends JPanel
 		this.baseLayout = new SpringLayout();
 		this.rightButton = new JButton("This is a spot you can press to affect the game.");
 		this.leftButton = new JButton("This is a spot you can press to affect the game.");
-		this.storyBox = new JLabel("Press either button to start the game.");
+		this.storyBox = new JLabel("<html>This is a mix of the two games Reigns and LifeLine both amazing. <br>You will have text show up on the screen and you press buttons to affect the story.<br> Press either button to start the game.<html>");
 		this.strangeLabel = new JLabel();
+		//this.mapLabel = new JLabel();
 		this.healthLabel = new JLabel("HEALTH: ");
 		this.foodLabel = new JLabel("FOOD: ");
 		this.waterLabel = new JLabel("WATER: ");
 		this.strangeGround = new ImageIcon(getClass().getResource("/strange/view/images/mountainBackdrop.jpg"));
+		this.newsPaper = new ImageIcon(getClass().getResource("/strange/view/images/SSWaveForm1.png"));
+		//this.userMap = new ImageIcon(getClass().getResource("/strange/view/images/Map.jpg"));
 		health = 50;
 		water = 100;
 		food = 100;
@@ -60,6 +67,9 @@ public class StrangePanel extends JPanel
 		this.add(healthLabel);
 		this.add(foodLabel);
 		this.add(waterLabel);
+		//this.mapLabel = new JLabel();
+		//this.add(mapLabel);
+		//mapLabel.setIcon(userMap);
 		this.add(strangeLabel);
 		strangeLabel.setIcon(strangeGround);
 		
@@ -69,9 +79,12 @@ public class StrangePanel extends JPanel
 	{
 		baseLayout.putConstraint(SpringLayout.NORTH, rightButton, 0, SpringLayout.NORTH, leftButton);
 		baseLayout.putConstraint(SpringLayout.SOUTH, rightButton, 0, SpringLayout.SOUTH, leftButton);
-		baseLayout.putConstraint(SpringLayout.NORTH, leftButton, 150, SpringLayout.SOUTH, storyBox);
 		storyBox.setFont(new Font("AR DARLING", Font.PLAIN, 16));
+		baseLayout.putConstraint(SpringLayout.WEST, rightButton, 707, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, leftButton, 500, SpringLayout.NORTH, this);
 		waterLabel.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		baseLayout.putConstraint(SpringLayout.NORTH, storyBox, 30, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, storyBox, -375, SpringLayout.SOUTH, this);
 		healthLabel.setFont(new Font("Tahoma", Font.PLAIN, 26));
 		foodLabel.setFont(new Font("Tahoma", Font.PLAIN, 26));
 		baseLayout.putConstraint(SpringLayout.NORTH, foodLabel, 200, SpringLayout.NORTH, this);
@@ -83,7 +96,6 @@ public class StrangePanel extends JPanel
 		strangeLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		baseLayout.putConstraint(SpringLayout.EAST, rightButton, -50, SpringLayout.EAST, this);
 		baseLayout.putConstraint(SpringLayout.EAST, leftButton, -707, SpringLayout.EAST, this);
-		baseLayout.putConstraint(SpringLayout.SOUTH, storyBox, -485, SpringLayout.SOUTH, this);
 		leftButton.setFont(new Font("AR CENA", Font.PLAIN, 17));
 		rightButton.setFont(new Font("AR CENA", Font.PLAIN, 17));
 		baseLayout.putConstraint(SpringLayout.NORTH, strangeLabel, 12, SpringLayout.NORTH, this);
@@ -91,7 +103,6 @@ public class StrangePanel extends JPanel
 		baseLayout.putConstraint(SpringLayout.WEST, leftButton, 50, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.SOUTH, strangeLabel, -12, SpringLayout.SOUTH, this);
 		baseLayout.putConstraint(SpringLayout.EAST, strangeLabel, -12, SpringLayout.EAST, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, storyBox, 100, SpringLayout.NORTH, this);
 		baseLayout.putConstraint(SpringLayout.WEST, storyBox, 200, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.EAST, storyBox, -200, SpringLayout.EAST, this);
 		storyBox.setHorizontalAlignment(SwingConstants.CENTER);
@@ -103,10 +114,13 @@ public class StrangePanel extends JPanel
 		{
 			public void actionPerformed(ActionEvent selection)
 			{
-				//This is only the start to changing the test and buttons so you can interact in the game.
-				storyBox.setText("");
+				if(level == 0)
+				{
+				storyBox.setIcon(newsPaper);
 				rightButton.setText("Continue");
 				leftButton.setText("Continue");
+				level++;
+				}
 			}
 
 		});
@@ -123,4 +137,3 @@ public class StrangePanel extends JPanel
 		});
 	}
 }
-//good start to your game man
