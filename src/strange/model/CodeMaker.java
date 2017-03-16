5424da6ea9a453e88f7d045f5bb01adc6e16036a
Asmi0816 <asmi0816@740s-pl-114.local>
2017-03-16 13:48:44 -0600

    Made the start of a mini game that will be used to defuse the bomb.

---
+++ src/strange/model/CodeMaker.java
@@ -0,0 +1,35 @@
+package strange.model;
+
+public class CodeMaker 
+{
+
+	private String alphabet = "abcdefghijklmnopqrstuvwxyz";
+	private String encodedAlphabet = "";
+	private String generateRandom;
+	
+	public String encodeMessage()
+	{
+		for(int i = 0; i < 25; i++)
+		{
+			
+		}
+		
+		return encodedAlphabet;
+	}
+	
+	private String randomMath()
+	{
+		int choiceNumber = 0;
+		generateRandom = "";
+		choiceNumber = (int)(Math.random() * 26);
+		if(choiceNumber == 1)
+		{
+			
+			
+		}
+		return generateRandom;
+	}
+	
+	
+	
+}
